bdd1b6721e7bbfacaeb184e7e1f4dea1e3ab7f01
FBosso <francesco.bosso@mail.polimi.it>
2026-08-22 09:32:38 +0200

    [hygene] remove unused code

--- nodes.py
+++ nodes.py
@@ -9,9 +9,3 @@ def agent(state: AgentState) -> AgentState:
     """Node invoking the llm with available tools"""
     response = llm_with_tools.invoke(state['messages'])
     return {"messages":[response]}
-
-def should_continue(state: AgentState) -> str:
-    if state['messages'][-1] == 'exit':
-        return 'end'
-    else:
-        return "continue"
