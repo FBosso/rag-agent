00ca80f919c07664b3869cf5b00ff649f1ee8ffb
FBosso <francesco.bosso@mail.polimi.it>
2026-08-22 11:17:41 +0200

    comments addition

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ def main():
 
     while True:
         user_input = input("Human: ")
-
+        #terminating condition
         if user_input.lower().strip() == "exit":
             print("🤖 Agent: See you next time!")
             break
-
+        
         events = app.stream(
-            {"messages": [("user", user_input)]},
-            stream_mode="values"
+            {"messages": [("user", user_input)]}, #input to the graph
+            stream_mode="values" #returns the entire state after every node
         )
 
         for event in events:
-            last_message = event["messages"][-1]
+            last_message = event["messages"][-1] #select the last message
 
         if last_message.type == "ai" and not last_message.tool_calls:
-            print(f"🤖 Agent: {last_message.content}\n")
+            print(f"🤖 Agent: {last_message.content}\n") #if it is ai i print it
 
 
 if __name__ == "__main__":
